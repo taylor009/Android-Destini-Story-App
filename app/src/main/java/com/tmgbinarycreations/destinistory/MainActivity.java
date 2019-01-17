006c8cfc6674d2333dbd3b523d326dcb4b22db8f
taylor009 <taylor@baremetalcloud.com>
2019-01-17 00:51:40 -0500

    Removed buttons from view after reaching an ending

--- app/src/main/java/com/tmgbinarycreations/destinistory/MainActivity.java
+++ app/src/main/java/com/tmgbinarycreations/destinistory/MainActivity.java
@@ -29,9 +29,16 @@ public class MainActivity extends AppCompatActivity {
         mTopButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mStoryTextView.setText(R.string.T3_Story);
-                mTopButton.setText(R.string.T3_Ans1);
-                mBottomButton.setText(R.string.T3_Ans2);
+                if (mStoryIndex == 1 || mStoryIndex == 2) {
+                    mStoryTextView.setText(R.string.T3_Story);
+                    mTopButton.setText(R.string.T3_Ans1);
+                    mBottomButton.setText(R.string.T3_Ans2);
+                    mStoryIndex = 3;
+                } else if (mStoryIndex == 3){
+                    mStoryTextView.setText(R.string.T6_End);
+                    mTopButton.setVisibility(View.GONE);
+                    mBottomButton.setVisibility(View.GONE);
+                }
             }
         });
         mBottomButton.setOnClickListener(new View.OnClickListener() {
@@ -44,6 +51,10 @@ public class MainActivity extends AppCompatActivity {
                     mStoryIndex = 2;
                 } else if (mStoryIndex == 2){
                     mStoryTextView.setText(R.string.T4_End);
+                    mTopButton.setVisibility(View.GONE);
+                    mBottomButton.setVisibility(View.GONE);
+                } else if (mStoryIndex == 3){
+                    mStoryTextView.setText(R.string.T5_End);
                     mTopButton.setVisibility(View.GONE);
                     mBottomButton.setVisibility(View.GONE);
                 }
