f4e6ad3fba7c5759c6407d8c7f20a0740ff4c543
taylor009 <taylor@baremetalcloud.com>
2019-01-16 23:52:08 -0500

    first commit

---
+++ app/src/main/java/com/tmgbinarycreations/destinistory/MainActivity.java
@@ -0,0 +1,25 @@
+package com.tmgbinarycreations.destinistory;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // TODO: Step 5 - Wire up the 3 views from the layout to the member variables:
+
+
+
+        // TODO: Steps 6, 7, & 9 - Set a listener on the top button:
+
+
+
+
+        // TODO: Steps 6, 7, & 9 - Set a listener on the bottom button:
+
+    }
+}
